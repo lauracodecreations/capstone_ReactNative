9d9dfccdfef6cb9fc72849c68f3ff305787b9dcf
Ada <larmartinez2@gmail.com>
2019-01-02 16:08:40 -0800

    create barcode scanner

--- screens/AddProduct.js
+++ screens/AddProduct.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { Constants, BarCodeScanner, Permissions } from 'expo';
 
 export default class AddProduct extends React.Component {
   static navigationOptions = {
     title: 'Add Product',
   };
 
+  state = {
+    hasCameraPermission: null
+  };
+
+  componentDidMount() {
+    this._requestCameraPermission();
+  }
+
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+  _handleBarCodeRead = data => {
+    Alert.alert(
+      'Scan successful!',
+      JSON.stringify(data)
+    );
+  };
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-      </ScrollView>
+        <View style={styles.container}>
+          {this.state.hasCameraPermission === null ?
+            <Text>Requesting for camera permission</Text> :
+            this.state.hasCameraPermission === false ?
+              <Text>Camera permission is not granted</Text> :
+              <BarCodeScanner
+                onBarCodeRead={this._handleBarCodeRead}
+                style={{ height: 200, width: 200 }}
+              />
+          }
+        </View>
     );
   }
 }
@@ -17,7 +49,9 @@ export default class AddProduct extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
 });

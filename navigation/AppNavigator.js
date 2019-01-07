0e13eccb40fcdfa477a28c7ed8ee1f65ef6c9894
Ada <larmartinez2@gmail.com>
2019-01-07 12:19:38 -0800

    refactor code for routes

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,57 +2,13 @@ import React from 'react';
 import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import ProductList from '../screens/ProductList';
 import EditProduct from '../screens/EditProduct';
-import AddProduct from '../screens/AddProduct';
 
 import MainTabNavigator from './MainTabNavigator';
-
-const BookcaseStack = createStackNavigator({
-    Bookcase: {
-        screen: ProductList,
-        navigationOptions: ({navigation}) => ({
-            header: null,
-        }),
-    },
-    EditProduct: {
-        screen: EditProduct,
-        navigationOptions: ({navigation}) => ({
-            header: null,
-            tabBarVisible: false,
-            gesturesEnabled: false
-        }),
-    },
-});
-
-const createRootNavigator = () => {
-    return createStackNavigator(
-        {
-            Tabs: {
-                screen: MainTabNavigator,
-                navigationOptions: ({navigation}) => ({
-                    gesturesEnabled: false,
-                })
-            },
-            BookcaseStack: {
-                screen: BookcaseStack,
-                navigationOptions: ({navigation}) => ({
-                    gesturesEnabled: false,
-                })
-            }
-
-        },
-        {
-            headerMode: "none",
-            mode: "modal"
-        }
-    );
-};
-
-const AppStack = createStackNavigator({ EditBook: EditProduct, ProductList: ProductList });
-
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  EditProduct: AppStack,
+  EditProduct: EditProduct,
+  ProductList: ProductList,
   Main: MainTabNavigator,
 });
